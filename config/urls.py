809f3c55ffd74e06d895cd6b8aca52ebd6fb2b59
yuripbong <kyri200350@gmail.com>
2025-07-16 23:08:55 +0900

    setting: uv 설정

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,5 @@
 """
 URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
@@ -20,26 +7,29 @@ from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# Swagger 설정
 schema_view = get_schema_view(
     openapi.Info(
-        title="Title",
+        title="TeamA API",
         default_version='v1',
-        description="Test description",
-        terms_of_service="<https://www.google.com/policies/terms/>",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
+        description="TeamA Django REST API Documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="team@example.com"),
+        license=openapi.License(name="MIT License"),
     ),
     public=True,
     permission_classes=[AllowAny],
 )
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('api/v1/', include('post.urls')),
-
-    re_path(r'^swagger(?P<format>\\.json|\\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
+    # API 엔드포인트
+    path('api/v1/', include('apps.posts.urls')),
+    
+    # Swagger API 문서
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # 루트에서도 Swagger 접근
 ]
