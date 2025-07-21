36c536366e2983f3406ddf21f706360a5e1d2057
조성환 <llsung0409@naver.com>
2025-07-22 00:28:20 +0900

    feat: 영상 삭제

--- apps/videos/urls.py
+++ apps/videos/urls.py
@@ -1,6 +1,8 @@
+# apps/videos/urls.py
 from django.urls import path
-from .views import UserVideoListView
+from .views import UserVideoListView, VideoDeleteView
 
 urlpatterns = [
     path('', UserVideoListView.as_view(), name='user-video-list'),
+    path('<int:video_id>', VideoDeleteView.as_view(), name='video-delete'),
 ]
--- apps/videos/views.py
+++ apps/videos/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .models import Video
 from .serializers import VideoSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class UserVideoListView(APIView):
     permission_classes = [IsAuthenticated]
@@ -24,3 +25,21 @@ class UserVideoListView(APIView):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class VideoDeleteView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, video_id):
+        user = request.user
+        try:
+            video = Video.objects.get(id=video_id, user=user)
+        except Video.DoesNotExist:
+            return Response(
+                {"message": "해당 영상이 없거나 권한이 없습니다."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        video.delete()
+        return Response(
+            {"id": video_id, "message": "영상이 성공적으로 삭제되었습니다"},
+            status=status.HTTP_200_OK
+        )
