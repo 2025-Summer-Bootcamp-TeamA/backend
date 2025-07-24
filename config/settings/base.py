59d76226c8ce2845a279cae1c1ad9fa7236985a5
Yeong gyun Jang <uliveonce9892@gmail.com>
2025-07-24 22:55:18 +0900

    feat: 비동기처리 도커 연동(django안깔리는 오류 해결 필요)

--- config/settings/base.py
+++ config/settings/base.py
@@ -135,3 +135,10 @@ LOGGING = {
 
 # OpenAI API 설정
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Seoul'
